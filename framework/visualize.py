65ee8ade6d10dc6e23886d6693c7d3da74c151c2
Alex Kosov <kosov.av@phystech.edu>
2019-04-19 23:33:13 +0300

    Fixed matplitlib mode (added support of grey images)

--- framework/visualize.py
+++ framework/visualize.py
@@ -7,7 +7,7 @@ FILE_NUM_TO_SAVE = 0
 MAX_SIZE_TO_SHOW = 960
 
 
-def visualize_image(image, title="", mode="show", inplace=False):
+def visualize_image(image, title="", mode="both", inplace=False):
     """
     Function for visualizing image
     :param image: image, which was read by opencv
@@ -20,8 +20,12 @@ def visualize_image(image, title="", mode="show", inplace=False):
         plt.title(title)
         image_to_show = image
         if len(image.shape) > 2:
+            assert(image.shape[2] == 3)
             image_to_show = image[:, :, ::-1]
-        plt.imshow(image_to_show)
+            plt.imshow(image_to_show)
+        else:
+            assert(len(image.shape) == 2)
+            plt.imshow(image_to_show, cmap='gray')
 
     else:
         image_to_show = image
@@ -39,7 +43,7 @@ def visualize_image(image, title="", mode="show", inplace=False):
                 new_size = tuple(map(lambda x: int(x), image_to_show.shape[:2][::-1] / ratio))
                 image_to_show = cv2.resize(image_to_show, new_size,
                                            interpolation=cv2.INTER_CUBIC)
-                title += f"Real size is {round(ratio, 2)} larger"
+                title += "Real size is {} larger".format(round(ratio, 2))
 
             cv2.imshow(title, image_to_show)
             cv2.waitKey(0)
